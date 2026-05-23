420adc385c523af312d8fa110f8147508d6946a6
Alessio Giusti <lello.giusti.99@gmail.com>
2026-05-23 15:01:57 +0200

    Remove transaction on cascade after deleting an account

--- src/api/accounts.ts
+++ src/api/accounts.ts
@@ -5,6 +5,8 @@ import type {
 	CreateAccountRequest,
 	UUID,
 } from "./types";
+import { useAccountsStore } from "@/stores/accounts.ts";
+import { useTransactionsStore } from "@/stores/transactions.ts";
 
 export const accountsApi = {
 	async create(payload: CreateAccountRequest): Promise<Account> {
@@ -40,5 +42,9 @@ export const accountsApi = {
 
 	async delete(accountId: UUID): Promise<void> {
 		await http.delete(`/accounts/${accountId}`);
+
+		// Remove transactions associated with the account
+		const transactionStore = useTransactionsStore();
+		transactionStore.transactions = transactionStore.transactions.filter(t => t.accountId !== accountId);
 	}
 };
